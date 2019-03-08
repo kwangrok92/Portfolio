10b14728b46a7882e15b07c57dafcd51426c7889
Kwangrok Kim <36951242+kwangrok92@users.noreply.github.com>
2019-03-09 01:07:06 +0900

    Update extract_img_from_video.py

--- extract_img_from_video.py
+++ extract_img_from_video.py
@@ -1,8 +1,8 @@
 import os
 import cv2
 
-# 저장할 폴더 생성
-save_dir = './cap_video'
+# 저장할 폴더 생성 (있으면 line6 주석처리)
+save_dir = './cap_video2'
 # os.mkdir(save_dir)
 
 # 이미지 추출 from 동영상
@@ -16,6 +16,9 @@ while True:
 
     # 이미지 저장
     if count % 10 == 0:
-        cv2.imwrite(os.path.join(save_dir, "frame{:d}.jpg".format(count)), image)  # save frame as JPEG file
-        print('Write a new frame:', count+1)
+        cv2.imwrite(os.path.join(save_dir, "frame{:d}.jpg".format(count+1)), image)  # save frame as JPEG file
+        print('Capturing img... please wait')
     count += 1
+
+    if count >= 12354:
+        print('Completed')
