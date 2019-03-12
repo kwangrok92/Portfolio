3f1660a8e55c01b356d7f1879a78d285fcd07226
Kwangrok Kim <36951242+kwangrok92@users.noreply.github.com>
2019-03-12 16:00:38 +0900

    Add files via upload

---
+++ crop_face.py
@@ -0,0 +1,38 @@
+from PIL import Image
+import face_recognition
+import os
+
+read_path = "./cap_video3/"
+write_path = "./cap_video3_crop/"
+n_face = 0
+
+img = os.listdir(read_path)
+
+for i in range(1, len(img)+1):
+
+    # Load the jpg file into a numpy array
+    image = face_recognition.load_image_file(os.path.join(read_path, img[i - 1]))
+
+    # Find all the faces in the image using the default HOG-based model.
+    face_locations = face_recognition.face_locations(image)
+
+    print("I found {} face(s) in this photograph.".format(len(face_locations)))
+    n_face += len(face_locations)
+
+    for j, face_location in enumerate(face_locations):
+        # Print the location of each face in this image
+        top, right, bottom, left = face_location
+        # print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
+
+        # crop the image
+        face_image = image[top:bottom, left:right]
+        pil_image = Image.fromarray(face_image)
+
+        # resize the image
+        pil_image_rs = pil_image.resize(size=(64, 64), resample=0, box=None)
+
+        # write the image
+        write_dir = write_path
+        pil_image_rs.save(write_dir + "taeim_" + str(i) + '_' + str(j + 1) + ".jpg")
+
+print(n_face)
