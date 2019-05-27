51511ff491b7e123bb0b4b2e7e413aac0009e0d8
Kwangrok Kim <36951242+kwangrok92@users.noreply.github.com>
2019-05-27 17:31:17 +0900

    Update FLD_dlib_cam_index.py

--- FLD_dlib_cam_index.py
+++ FLD_dlib_cam_index.py
@@ -1,7 +1,9 @@
 import cv2
 import dlib
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)  # Cam
+cap = cv2.VideoCapture("Zuckerburg.mp4")  # Input the Video
+# cap.set(cv2.CAP_PROP_FPS, 100)
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
@@ -26,9 +28,9 @@ while True:
             cv2.putText(frame, str(n), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))  # 랜드마크 숫자로 찍기
             # cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)  # 랜드마크 점으로 찍기
 
-    # cv2.imshow("Frame", frame)  # 원본
-    frame_rs = cv2.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
-    cv2.imshow("Frame", frame_rs)  # 1.5배 확대
+    cv2.imshow("Face Landmark Detector", frame)  # 원본
+    # frame_rs = cv2.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
+    # cv2.imshow("Frame", frame_rs)  # 1.5배 확대
 
     # 아무 키를 누르면 창 종료 (키를 누르지 않으면 -1 이 반환되기 때문에)
     if cv2.waitKey(10) > -1:
